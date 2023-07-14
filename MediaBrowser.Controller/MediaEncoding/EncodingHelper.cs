d1225bc398b49c028e69ae22dd37dd108d25c5c2
holow29 <67209066+holow29@users.noreply.github.com>
2023-07-14 10:12:23 -0400

    Test removal of shifting of DTS and TrueHD

--- MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
+++ MediaBrowser.Controller/MediaEncoding/EncodingHelper.cs
@@ -5921,8 +5921,8 @@ namespace MediaBrowser.Controller.MediaEncoding
             {
                 // DTS and TrueHD are not supported by HLS
                 // Keep them in the supported codecs list, but shift them to the end of the list so that if transcoding happens, another codec is used
-                shiftAudioCodecs.Add("dca");
-                shiftAudioCodecs.Add("truehd");
+                //shiftAudioCodecs.Add("dca");
+                //shiftAudioCodecs.Add("truehd");
             }
             else
             {
